619857adff4b9144dd03ca437e711237ed59c00a
sara pulido <sara01rizo@gmail.com>
2023-06-13 01:14:17 +0200

    Mod: an approach to use observable in a better way

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import {TASKS} from '../mock-tasks';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
 import {Task} from '../Task';
 
 
@@ -9,11 +9,11 @@ import {Task} from '../Task';
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   getTask(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
