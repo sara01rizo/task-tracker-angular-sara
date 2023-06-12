daae58bdb262b0c24a431dded374ed798a832751
sara pulido <sara01rizo@gmail.com>
2023-06-13 00:56:56 +0200

    Set backend mock in db json connected to services

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {TASKS} from '../../mock-tasks';
+import { TaskService } from 'src/app/services/task.service';
 import {Task} from '../../Task';
 
 @Component({
@@ -8,10 +8,12 @@ import {Task} from '../../Task';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.taskService.getTask().subscribe((tasks) => (this.tasks= tasks));
+  }
 
 }
